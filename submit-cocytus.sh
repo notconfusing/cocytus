a214d9405dd9ed06c8f814d18e22922bee3932e0
cocytus <none@example.info>
2015-03-27 23:38:19 +0000

    change to burst processing to better handle load

--- submit-cocytus.sh
+++ submit-cocytus.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 #submit the input
-#take burst mode off the commandline
 
 EMAIL=di.franco@gmail.com
+DIR=/data/project/cocytus
+PYTHONPATH=$DIR/c-env/local/lib/python2.7/site-packages
+source $DIR/c-env/bin/activate
 
 for n in {input,output}
 do
-    jstart -m ae -N "cocytus-$n" -mem 2g -e /data/project/cocytus/logs -o /data/project/cocytus/logs -continuous /data/project/cocytus/c-env/bin/python /data/project/cocytus/workspace/cocytus-$n.py 
+    jstart -N "cocytus-$n" -mem 2g -e $DIR/logs -o $DIR/logs\
+     -continuous -l release=trusty python $DIR/workspace/cocytus-$n.py
 done
 #launch somes workers on changes
 for worker in {changes,failed}
 do
     for i in {1..12}
     do
-	jstart -m ae -N "$worker$i" -mem 2g -e /data/project/cocytus/logs -o /data/project/cocytus/logs /data/project/cocytus/c-env/bin/rqworker -H tools-redis $worker
+	jstart -N "$worker$i" -mem 2g -e $DIR/logs -o $DIR/logs\
+	 -continuous -l release=trusty rqworker --burst -u http://tools-redis:6379 $worker
     done
 done
 
